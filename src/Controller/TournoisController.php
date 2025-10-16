c2b39c85d77abb83295218a2b9c27e9dd1eeada7
kamdinee <kamdine.hassanzadeh@outlook.com>
2025-10-16 16:58:58 +0200

    fix container fluid

--- src/Controller/TournoisController.php
+++ src/Controller/TournoisController.php
@@ -12,7 +12,8 @@ final class TournoisController extends AbstractController
     public function index(): Response
     {
         return $this->render('tournois/index.html.twig', [
-            'controller_name' => 'TournoisController', 'menuActif' => 'Jeux'
+            'controller_name' => 'TournoisController', 
+            'menuActif' => 'Jeux'
         ]);
     }
 }
